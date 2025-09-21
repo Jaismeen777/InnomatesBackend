2e16c0faf18fc86623a98c2960864ff6734df931
Jaismeen777 <jaissawhney123@gmail.com>
2025-09-21 22:21:38 +0530

    Updtae

--- genai.py
+++ genai.py
@@ -178,4 +178,7 @@ def analyze_face():
             os.remove(img_path)
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
+
